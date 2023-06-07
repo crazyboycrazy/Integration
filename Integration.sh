2aa2f7909858551471592515de7ed84d0b0e366d
crazyboycrazy <89303283+crazyboycrazy@users.noreply.github.com>
2023-06-07 14:02:21 +0900

    Update Integration.sh

--- Integration.sh
+++ Integration.sh
@@ -24,3 +24,7 @@ seqkit grep -f bacteria_name  ../Bacteria_genome/library.fna  > Bacteria_library
 cat Bacteria_library.fna  ../Zebrafish_genome/GCA_903798175.1_fDreNAz1.1_genomic.fna  > merged_reference.fa
 #3) make blast db
 blast/ncbi-blast-2.14.0+/bin/makeblastdb -in merged_reference.fa -dbtype nucl -out output_db
+
+
+#Blast
+blastn  -query ../kimeric_read.fa  -subject ../blast_reference/dmel_bacteria.fa  -outfmt "7 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen"  -evalue 1e-6  -task megablast > blast_out_result
