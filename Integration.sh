4fe08470238559392e313c1ce6063ffcd3289951
crazyboycrazy <89303283+crazyboycrazy@users.noreply.github.com>
2023-06-09 14:06:43 +0900

    Update Integration.sh

--- Integration.sh
+++ Integration.sh
@@ -22,7 +22,7 @@ seqkit fq2fa  extracted.fastq  > extracted.fasta
 #Make blast reference
 #1) extract bacteria fasta
 less output_minimap.sam |cut -f 3|grep kraken|sort|uniq > bacteria_name
-seqkit grep -f bacteria_name  ../Bacteria_genome/library.fna  > Bacteria_library.fna
+seqkit grep -f bacteria_name  ../normal/Bacteria_genome/library.fna  > Bacteria_library.fna
 #2) merge bacteria and zebrafish fasta
 cat Bacteria_library.fna  ../Zebrafish_genome/GCA_903798175.1_fDreNAz1.1_genomic.fna  > merged_reference.fa
 
