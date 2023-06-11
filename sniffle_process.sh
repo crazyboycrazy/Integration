5438d2330e18c33496551547e41b30be7b1caf61
crazyboycrazy <89303283+crazyboycrazy@users.noreply.github.com>
2023-06-11 22:46:26 +0900

    Update sniffle_process.sh

--- sniffle_process.sh
+++ sniffle_process.sh
@@ -2,3 +2,10 @@
 ../../minimap2-2.26_x64-linux/minimap2 -ax map-ont -t 10 MEtronidazole_merged.fa  normal_zebra_extracted.fastq > output_minimap.sam
 
 #2
+samtools view -b output_minimap.sam  -o  output_minimap.bam
+
+#3
+samtools sort output_minimap.bam > output_minimap.sorted.bam
+
+#4
+samtools index output_minimap.sorted.bam
